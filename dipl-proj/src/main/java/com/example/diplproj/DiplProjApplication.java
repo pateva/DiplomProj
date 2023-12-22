7cb6e0910d35ed0aae1da5cb3d08b246df30b415
Petya Ateva <petya.ateva@cleverpine.com>
2023-12-22 13:10:35 +0200

    Initial project setup

---
+++ dipl-proj/src/main/java/com/example/diplproj/DiplProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.diplproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplProjApplication.class, args);
+	}
+
+}
---
+++ dipl-proj/src/test/java/com/example/diplproj/DiplProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
